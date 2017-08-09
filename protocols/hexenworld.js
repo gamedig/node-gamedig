14aa56714f65b88088ec19016566faa051349bbe
mmorrison <mmorrison@pon4.net>
2017-08-09 04:36:38 -0500

    Fixed hexen 2 and heretic 2 (finally found some servers to test against)

--- protocols/hexenworld.js
+++
@@ -1,9 +0,0 @@
-class HexenWorld extends require('./quake1') {
-	constructor() {
-		super();
-		this.sendHeader = '\xFFstatus\x0a';
-        this.responseHeader = '\xffn';
-	}
-}
-
-module.exports = HexenWorld;
