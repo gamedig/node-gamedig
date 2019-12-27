9db659b86bedaac81cdfe37593d68fe361ae2adf
cwbr <xchris32x@live.de>
2019-12-27 19:53:38 +0100

    Add Assetto Corsa support

---
+++ protocols/assettocorsa.js
@@ -0,0 +1,41 @@
+const Core = require('./core');
+
+class AssettoCorsa extends Core {
+    async run(state) {
+        const serverInfo = await this.request({
+            json: true,
+            uri: `http://${this.options.address}:${this.options.port}/INFO`
+        });
+        const carInfo = await this.request({
+            json: true,
+            uri: `http://${this.options.address}:${this.options.port}/JSON|${parseInt(Math.random() * 999999999999999, 10)}`
+        });
+
+        if (!serverInfo || !carInfo || !carInfo.Cars) {
+            throw new Error('Query not successful');
+        }
+        
+        state.maxplayers = serverInfo.maxclients;
+        state.name = serverInfo.name;
+        state.map = serverInfo.track;
+        state.password = serverInfo.pass;
+        state.gamePort = serverInfo.port;
+        state.raw.carInfo = carInfo.Cars;
+        state.raw.serverInfo = serverInfo;
+
+        state.players = carInfo.Cars.reduce((r, e) => {
+            if (e.IsConnected) {
+                r.push({
+                    name: e.DriverName,
+                    car: e.Model,
+                    skin: e.Skin,
+                    nation: e.DriverNation,
+                    team: e.DriverTeam
+                });
+            }
+            return r;
+        }, state.players);
+    }
+}
+
+module.exports = AssettoCorsa;
