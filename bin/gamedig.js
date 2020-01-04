5dd7446eaeb8416f97f66185a638ff728bb7942f
Tom <25043847+Douile@users.noreply.github.com>
2020-01-04 01:03:48 +0000

    Force host argument to be string (Int causes error)

--- bin/gamedig.js
+++ bin/gamedig.js
@@ -4,7 +4,8 @@ const Minimist = require('minimist'),
     Gamedig = require('..');
 
 const argv = Minimist(process.argv.slice(2), {
-    boolean: ['pretty','debug']
+    boolean: ['pretty','debug'],
+    string: ['_']
 });
 
 const debug = argv.debug;
