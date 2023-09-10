f79486bf553e76504ceaef2494fa9d90ebed3cb2
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-11 01:28:22 +0200

    Update cs2d.js

--- protocols/cs2d.js
+++ protocols/cs2d.js
@@ -4,47 +4,41 @@ class Cs2d extends Core {
     async run(state) {
         {
             const reader = await this.sendQuery(
-                Buffer.from('\x01\x00\x03\x10\x21\xFB\x01\x75\x00', 'binary'),
-                Buffer.from('\x01\x00\xfb\x01', 'binary')
+                Buffer.from('\x01\x00\xFB\x01\xF5\x03\xFB\x05', 'binary'),
+                Buffer.from('\x01\x00\xFB\x01', 'binary')
             );
-            const flags = reader.uint(1);
-            state.raw.flags = flags;
+            let flags = reader.uint(1);
             state.password = this.readFlag(flags, 0);
             state.raw.registeredOnly = this.readFlag(flags, 1);
             state.raw.fogOfWar = this.readFlag(flags, 2);
             state.raw.friendlyFire = this.readFlag(flags, 3);
             state.raw.botsEnabled = this.readFlag(flags, 5);
             state.raw.luaScripts = this.readFlag(flags, 6);
+            state.raw.forceLight = this.readFlag(flags, 7);
             state.name = this.readString(reader);
             state.map = this.readString(reader);
             state.raw.numplayers = reader.uint(1);
             state.maxplayers = reader.uint(1);
-            state.raw.gamemode = reader.uint(1);
-            if (state.raw.botsEnabled) {
-                state.raw.numbots = reader.uint(1);
+            if (flags & 32) {
+                state.raw.gamemode = reader.uint(1);
             } else {
-                state.raw.numbots = 0;
+                state.raw.gamemode = 0;
             }
-        }
-
-        {
-            const reader = await this.sendQuery(
-                Buffer.from('\x01\x00\xFB\x05', 'binary'),
-                Buffer.from('\x01\x00\xFB\x05', 'binary')
-            );
-            state.raw.numplayers2 = reader.uint(1);
-            while(!reader.done()) {
-                const player = {};
+            state.raw.numbots = reader.uint(1);
+            flags = reader.uint(1);
+            state.raw.recoil = this.readFlag(flags, 0);
+            state.raw.offScreenDamage = this.readFlag(flags, 1);
+            state.raw.hasDownloads = this.readFlag(flags, 2);
+            reader.skip(2);
+            const players = reader.uint(1);
+            for (let i = 0; i < players; i++) {
+                const player = {}
                 player.id = reader.uint(1);
                 player.name = this.readString(reader);
                 player.team = reader.uint(1);
                 player.score = reader.uint(4);
                 player.deaths = reader.uint(4);
-                if (state.bots.length < state.raw.numbots) {
-                    state.bots.push(player);
-                } else {
-                    state.players.push(player);
-                }
+                state.players.push(player);
             }
         }
     }
