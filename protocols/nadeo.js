0a4926aa84c442d311202c0743e6a42364776c33
CosminPerRam <cosmin.p@live.com>
2023-09-14 17:08:34 +0300

    Remove remaining module.exports

--- protocols/nadeo.js
+++ protocols/nadeo.js
@@ -2,7 +2,7 @@ import Core from './core';
 import { Promises} from "../lib/Promises";
 import * as gbxremote from 'gbxremote';
 
-class Nadeo extends Core {
+export class Nadeo extends Core {
     async run(state) {
         await this.withClient(async client => {
             const start = Date.now();
@@ -82,7 +82,4 @@ class Nadeo extends Core {
     stripColors(str) {
         return str.replace(/\$([0-9a-f]{3}|[a-z])/gi,'');
     }
-
 }
-
-module.exports = Nadeo;
