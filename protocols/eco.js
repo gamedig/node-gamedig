9b659fddc6d75a7da0d097ddfb110f43f6e48b26
CosminPerRam <cosmin.p@live.com>
2023-09-17 00:54:34 +0300

    Add initial working eco query

---
+++ protocols/eco.js
@@ -0,0 +1,19 @@
+import Core from './core.js';
+
+export default class eco extends Core {
+    async run(state) {
+        const serverInfo = await this.request({
+            url: 'http://'+this.options.address+':'+this.options.port+'/frontpage',
+            responseType: 'json'
+        });
+
+        for (const key of Object.keys(serverInfo)) {
+            state.raw[key] = serverInfo[key];
+        }
+
+        state.name = serverInfo.Description;
+        state.maxplayers = serverInfo.TotalPlayers;
+        state.password = serverInfo.HasPassword;
+        state.gamePort = serverInfo.GamePort;
+    }
+}
