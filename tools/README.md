3a1950133d5ee5c47ddeb475a50b7c8557bf7faa
CosminPerRam <cosmin.p@live.com>
2023-10-30 23:28:21 +0200

    chore: Move the gen readme in a separate folder than bin

---
+++ tools/README.md
@@ -0,0 +1,2 @@
+This folder is not really intended to be used by users.
+Contains helpers for certain stuff (such as generating the games list Markdown table).
---
+++ tools/genreadme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import * as fs from 'node:fs'
+import GameResolver from '../lib/GameResolver.js'
+
+const gameResolver = new GameResolver()
+
+const generated = gameResolver.printReadme()
+
+const readmeFilename = __dirname + '/../README.md'
+const readme = fs.readFileSync(readmeFilename, { encoding: 'utf8' })
+
+const marker_top = '<!--- BEGIN GENERATED GAMES -->'
+const marker_bottom = '<!--- END GENERATED GAMES -->'
+
+let start = readme.indexOf(marker_top)
+start += marker_top.length
+const end = readme.indexOf(marker_bottom)
+
+const updated = readme.substring(0, start) + '\n\n' + generated + '\n' + readme.substring(end)
+fs.writeFileSync(readmeFilename, updated)
