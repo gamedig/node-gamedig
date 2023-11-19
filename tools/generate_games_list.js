aa6d65e8085936e809c765188bd7e184ad7773fa
CosminPerRam <cosmin.p@live.com>
2023-11-19 02:33:30 +0200

    fix: generate games list md file

--- tools/generate_games_list.js
+++ tools/generate_games_list.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 
-/*
 import * as fs from 'node:fs'
-import GameResolver from '../lib/GameResolver.js'
+import { games } from '../lib/games.js'
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
-
-const gameResolver = new GameResolver()
-
-const generated = gameResolver.printReadme()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -19,10 +14,29 @@ const readme = fs.readFileSync(readmeFilename, { encoding: 'utf8' })
 const markerTop = '<!--- BEGIN GENERATED GAMES -->'
 const markerBottom = '<!--- END GENERATED GAMES -->'
 
+let generated = ''
+generated += '| GameDig Type ID | Name | See Also\n'
+generated += '|---|---|---\n'
+
+for (const id in games) {
+  const game = games[id]
+  generated += '| ' + id.padEnd(10, ' ') + ' | ' + game.name
+  const notes = []
+  if (game?.extra?.doc_notes) {
+    notes.push('[Notes](#' + game.extra.doc_notes + ')')
+  }
+  if (game.options.protocol === 'valve') {
+    notes.push('[Valve Protocol](#valve)')
+  }
+  if (notes.length) {
+    generated += ' | ' + notes.join(', ')
+  }
+  generated += '\n'
+}
+
 let start = readme.indexOf(markerTop)
 start += markerTop.length
 const end = readme.indexOf(markerBottom)
 
 const updated = readme.substring(0, start) + '\n\n' + generated + '\n' + readme.substring(end)
 fs.writeFileSync(readmeFilename, updated)
-*/
