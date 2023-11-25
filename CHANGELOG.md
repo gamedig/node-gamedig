e25fcf17bd06b0d8c6b05d6be2b49a02ea773196
CosminPerRam <cosmin.p@live.com>
2023-11-25 16:03:56 +0200

    Add `Eco` (2018) support. (#363)
    * Add initial working eco query
    
    * Add default port and add the game to games.txt
    
    * Add to changelog
    
    * Fix request Info object and add to raw the object
    
    * Rearrange some code to be more readable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,15 @@
 ### To Be Released...
 * (changelog)
 
+#### Other changes
+* Replaced usage of deprecated `substr` with `substring`.
+* Moved the library a `module`.
+* CLI: Resolved incorrect error message when querying with a non-existent protocol name.
+* Replaced deprecated internal `punycode` with the [punycode](https://www.npmjs.com/package/punycode) package.
+
+### 4.2.0
+* Eco (2018) - Added support (requested by @dgibbs64)
+
 ### 4.1.0
 * Replace `compressjs` dependency by `seek-bzip` to solve some possible import issues.
 * Sons Of The Forest (2023) - Added support
---
+++ protocols/eco.js
@@ -0,0 +1,19 @@
+import Core from './core.js';
+
+export default class eco extends Core {
+    async run(state) {
+        if (!this.options.port) this.options.port = 3001;
+
+        const request = await this.request({
+            url: `http://${this.options.address}:${this.options.port}/frontpage`,
+            responseType: 'json'
+        });
+        const serverInfo = request.Info;
+
+        state.name = serverInfo.Description;
+        state.maxplayers = serverInfo.TotalPlayers;
+        state.password = serverInfo.HasPassword;
+        state.gamePort = serverInfo.GamePort;
+        state.raw = serverInfo;
+    }
+}
