6bfbc883bee463fcc64988d7cfb424f2d31c8712
CosminPerRam <cosmin.p@live.com>
2023-12-30 22:41:56 +0200

    feat: add BeamMP support (#447)
    * feat: add beammp support
    
    * fix: cleanup server name of escape codes
    
    * chore: add a new line for better readability

---
+++ protocols/beammp.js
@@ -0,0 +1,32 @@
+import Core from './core.js'
+import beammpmaster from './beammpmaster.js'
+
+export default class beammp extends Core {
+  async run (state) {
+    const master = new beammpmaster()
+    master.options = this.options
+    const masterState = await master.runOnceSafe()
+    const servers = masterState.raw.servers
+    const server = servers.find(s => s.ip === this.options.host)
+
+    if (!server) {
+      throw new Error('Server not found in the master list')
+    }
+
+    state.name = server.sname.replace(/\^./g, '')
+    state.map = server.map
+    state.password = server.password
+    state.numplayers = parseInt(server.players)
+    state.maxplayers = parseInt(server.players)
+
+    const players = server.playerslist.split(';')
+    if (players[players.length - 1] === '') {
+      players.pop()
+    }
+    players.forEach(player => {
+      state.players.push({ name: player })
+    })
+
+    state.raw = server
+  }
+}
