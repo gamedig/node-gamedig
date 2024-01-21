db6d84ceb60f19706830271e4e2a7ba5cd90a9aa
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2024-01-20 23:26:57 -0300

    add option dontCheckOldIDs

--- bin/gamedig.js
+++ bin/gamedig.js
@@ -6,7 +6,7 @@ import Minimist from 'minimist'
 import { GameDig } from './../lib/index.js'
 
 const argv = Minimist(process.argv.slice(2), {
-  boolean: ['pretty', 'debug', 'givenPortOnly', 'requestRules', 'requestRulesRequired', 'requestPlayersRequired', 'stripColors', 'portCache', 'noBreadthOrder'],
+  boolean: ['pretty', 'debug', 'givenPortOnly', 'requestRules', 'requestRulesRequired', 'requestPlayersRequired', 'stripColors', 'portCache', 'noBreadthOrder', 'dontCheckOldIDs'],
   string: ['guildId', 'listenUdpPort', 'ipFamily'],
   default: {
     stripColors: true,
