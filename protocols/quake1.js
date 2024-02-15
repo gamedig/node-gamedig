5aa3fec546f975c0f324f2b0c13c29e1cf35bffe
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2024-02-14 21:53:51 -0300

    new game

--- protocols/quake1.js
+++ protocols/quake1.js
@@ -6,4 +6,9 @@ export default class quake1 extends quake2 {
     this.responseHeader = 'n'
     this.isQuake1 = true
   }
+
+  async run(state) {
+    await super.run(state)
+    if ('*version' in state.raw) state.version = state.raw['*version']
+  }
 }
