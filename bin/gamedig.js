f5899fd54d286f2bcd1181ab835a48a822b13d18
CosminPerRam <cosmin.p@live.com>
2024-10-16 22:40:06 +0300

    feat(games/satisfactory): pass optional token, make http query not required (#653)
    * feat: let the user provide a token to skip PasswordlessLogin
    
    * feat: make querying the HTTP api not required
    
    * docs: add to changelog, note query priority
    
    * fix: passing rejectUnauthorized as arg made it string, ignoring its value

--- bin/gamedig.js
+++ bin/gamedig.js
@@ -6,7 +6,7 @@ import Minimist from 'minimist'
 import { GameDig } from './../lib/index.js'
 
 const argv = Minimist(process.argv.slice(2), {
-  boolean: ['pretty', 'debug', 'givenPortOnly', 'requestRules', 'requestPlayers', 'requestRulesRequired', 'requestPlayersRequired', 'stripColors', 'portCache', 'noBreadthOrder', 'checkOldIDs'],
+  boolean: ['pretty', 'debug', 'givenPortOnly', 'requestRules', 'requestPlayers', 'requestRulesRequired', 'requestPlayersRequired', 'stripColors', 'portCache', 'noBreadthOrder', 'checkOldIDs', 'rejectUnauthorized'],
   string: ['guildId', 'serverId', 'listenUdpPort', 'ipFamily', 'token'],
   default: {
     stripColors: true,
