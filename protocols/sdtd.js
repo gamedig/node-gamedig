990710c43d67fbc5b2295654113af87645e0dd3e
CosminPerRam <cosmin.p@live.com>
2025-05-06 00:41:26 +0300

    feat(protocol/sdtd): add optional more data via telnet

--- protocols/sdtd.js
+++ protocols/sdtd.js
@@ -2,6 +2,7 @@ import Valve from './valve.js'
 import { Players } from '../lib/Results.js'
 
 const playerLineRegex = /(?<=id=\d+,\s*)(?<name>\S[^,]*)(?=,)/
+const modLineRegex = /^Mod\s+([^:]+):\s*([\d.]+)$/
 
 const sanitizeTelnetResponse = response => {
   return response
@@ -25,7 +26,7 @@ export default class sdtd extends Valve {
       return
     }
 
-    if (!this.options.requestPlayers) {
+    if (!this.options.requestPlayers && !this.options.moreData) {
       return
     }
 
@@ -37,7 +38,13 @@ export default class sdtd extends Valve {
       echoLines: 0
     })
 
-    await this.telnetCallPlayers(state)
+    if (this.options.requestPlayers) {
+      await this.telnetCallPlayers(state)
+    }
+
+    if (this.options.moreData) {
+      await this.telnetMoreData(state)
+    }
 
     await this.telnetClose()
   }
@@ -58,5 +65,43 @@ export default class sdtd extends Valve {
     }
 
     state.raw.telnetPlayersResponse = playersResponse
+  }
+
+  async telnetMoreData (state) {
+    const gettimeResponse = await this.telnetExecute('gettime')
+    const dateTime = sanitizeTelnetResponse(gettimeResponse)[0] || ''
+    const match = dateTime.match(/Day\s+(\d+),\s*(\d{2}:\d{2})/)
+
+    if (match) {
+      state.raw.day = Number(match[1])
+      state.raw.time = match[2]
+    }
+
+    state.raw.telnetGettimeResponse = gettimeResponse
+
+    const versionResponse = await this.telnetExecute('version')
+    const version = sanitizeTelnetResponse(versionResponse)
+    const gameVersions = version[0] || ''
+
+    const versions = gameVersions.match(/V \d+\.\d+(?: \(b\d+\))?/g)
+
+    if (versions) {
+      state.raw.gameVersion = versions[0]
+      state.raw.compatibilityVersion = versions[1]
+    }
+
+    const mods = []
+    for (const possibleMod of version.slice(1)) {
+      const match = possibleMod.match(modLineRegex)
+      if (match) {
+        mods.push({
+          name: match[1],
+          version: match[2]
+        })
+      }
+    }
+
+    state.raw.mods = mods
+    state.raw.telnetVersionResponse = versionResponse
   }
 }
