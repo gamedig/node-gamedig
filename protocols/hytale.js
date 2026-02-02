c4adb680405ef240eb0214e7dc98cb4dd22f6ae6
CosminPerRam <cosmin.p@live.com>
2026-02-03 01:18:30 +0200

    feat(protocol/hytale): add more data to raw, remove https, docs (#752)
    * feat: refactor to use http only
    
    * feat: place entire player data in raw field
    
    * feat: add basic data to raw
    
    * feat: remove rejectUnauthorized usage
    
    * docs: add GAMES_LIST.md entry
    
    * docs: add note about mods usage

--- protocols/hytale.js
+++ protocols/hytale.js
@@ -4,21 +4,9 @@ export default class hytale extends Core {
   async run (state) {
     this.usedTcp = true
 
-    // Hytale servers commonly use self-signed certificates for HTTPS.
-    // We default rejectUnauthorized to false unless explicitly set.
-    if (this.options.rejectUnauthorized === undefined) {
-      this.options.rejectUnauthorized = false
-    }
+    const response = await this.queryEndpoint()
 
-    let response
-    // Try HTTPS first (most common), fall back to HTTP
-    try {
-      response = await this.queryEndpoint('https')
-    } catch (e) {
-      this.logger.debug('HTTPS query failed, trying HTTP')
-      this.logger.debug(e)
-      response = await this.queryEndpoint('http')
-    }
+    state.raw.basic = response.Basic
 
     if (response.Server) {
       state.name = response.Server.Name
@@ -36,10 +24,7 @@ export default class hytale extends Core {
     if (response.Players) {
       state.players = response.Players.map(player => ({
         name: player.Name,
-        raw: {
-          uuid: player.UUID,
-          world: player.World
-        }
+        raw: player
       }))
     }
 
@@ -48,8 +33,8 @@ export default class hytale extends Core {
     }
   }
 
-  async queryEndpoint (protocol) {
-    const url = `${protocol}://${this.options.host}:${this.options.port}/Nitrado/Query`
+  async queryEndpoint () {
+    const url = `http://${this.options.host}:${this.options.port}/Nitrado/Query`
 
     const requestOptions = {
       url,
@@ -57,13 +42,6 @@ export default class hytale extends Core {
         Accept: 'application/json'
       },
       responseType: 'json'
-    }
-
-    if (protocol === 'https') {
-      requestOptions.https = {
-        minVersion: 'TLSv1.2',
-        rejectUnauthorized: this.options.rejectUnauthorized
-      }
     }
 
     return await this.request(requestOptions)
